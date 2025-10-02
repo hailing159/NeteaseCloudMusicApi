7b8b8180d5dec8fc71db3af6d463bcda407deb3f
hailing159 <142367722+hailing159@users.noreply.github.com>
2025-10-02 20:03:08 +0800

    index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
-const { createServer } = require('http')
-const app = require('../app')  
+const app = require('../app')
 
 module.exports = (req, res) => {
-  createServer(app.callback()).emit('request', req, res)
+  return app(req, res)
 }
