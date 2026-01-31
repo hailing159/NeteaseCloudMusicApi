d009ba312598ff3110e997735544670dde094506
hailing159 <142367722+hailing159@users.noreply.github.com>
2026-01-31 17:15:40 +0800

    Update playlist_detail.js

--- module/playlist_detail.js
+++ module/playlist_detail.js
@@ -4,7 +4,7 @@ module.exports = (query, request) => {
   const data = {
     id: query.id,
     n: 100000,
-    s: query.s || 0,
+    s: query.s || 5,
   }
   return request('POST', `https://music.163.com/api/v6/playlist/detail`, data, {
     crypto: 'api',
