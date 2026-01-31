0521a2a9f1580249c3572c93ce2e25c4395552d0
hailing159 <142367722+hailing159@users.noreply.github.com>
2026-01-31 17:10:58 +0800

    Set default value of 's' to 0 in playlist_detail.js

--- module/playlist_detail.js
+++ module/playlist_detail.js
@@ -4,7 +4,7 @@ module.exports = (query, request) => {
   const data = {
     id: query.id,
     n: 100000,
-    s: query.s || 8,
+    s: query.s || 0,
   }
   return request('POST', `https://music.163.com/api/v6/playlist/detail`, data, {
     crypto: 'api',
