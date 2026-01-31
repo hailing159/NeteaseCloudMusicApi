c9ba8ca984bceb580139a8ca2894774c54a5ff35
hailing159 <142367722+hailing159@users.noreply.github.com>
2026-01-31 17:17:34 +0800

    Remove default value for 's' in request data
    Remove default value for 's' in playlist detail request.

--- module/playlist_detail.js
+++ module/playlist_detail.js
@@ -4,7 +4,6 @@ module.exports = (query, request) => {
   const data = {
     id: query.id,
     n: 100000,
-    s: query.s || -1,
   }
   return request('POST', `https://music.163.com/api/v6/playlist/detail`, data, {
     crypto: 'api',
